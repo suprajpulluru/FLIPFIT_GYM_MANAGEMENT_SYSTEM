fe5bc39e4d09e54e8305139cc8c6d4b6e670bb92
suprajpulluru <suprajpulluru2004@gmail.com>
2025-08-20 10:28:13 +0530

    changes

--- JEDI-FLIPFIT-JAVA-POS/com/flipfit/bean/FlipFitAdmin.java
+++ JEDI-FLIPFIT-JAVA-POS/com/flipfit/bean/FlipFitAdmin.java
@@ -1,4 +1,5 @@
 package com.flipfit.bean;
 
 public class FlipFitAdmin {
+
 }
--- JEDI-FLIPFIT-JAVA-POS/com/flipfit/business/BookingService.java
+++ JEDI-FLIPFIT-JAVA-POS/com/flipfit/business/BookingService.java
@@ -1,4 +1,31 @@
 package com.flipfit.business;
 
+import com.flipfit.bean.*;
+import java.util.*;
+import java.util.Date;
+
 public class BookingService {
+
+    List<Booking> bookings=new ArrayList<>();
+    Date d1=new Date();
+
+
+    public boolean isConfirmed(String bookingId) {
+
+        for(Booking b:bookings)
+        {
+            if(b.getBookingId().equals(bookingId))
+            {
+                if(b.getType() == "confirmed")
+                    return true;
+                else
+                    return false;
+            }
+        }
+        return false;
+    }
+
+    public int getWaitingList() {
+        return -1;
+    }
 }
