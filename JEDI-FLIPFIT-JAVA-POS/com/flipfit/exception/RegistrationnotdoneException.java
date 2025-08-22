7436126c133c250aa4f3710acc385235bf6ff6e8
Harshita Kanwar <2021ucs0098@iitjammu.ac.in>
2025-08-22 10:34:09 +0530

    Exception Files

---
+++ JEDI-FLIPFIT-JAVA-POS/com/flipfit/exception/RegistrationnotdoneException.java
@@ -0,0 +1,13 @@
+package com.flipfit.exception;
+/*
+ *@Author : "Your Name"
+ *@ClassName: "RegistrationnotdoneException"
+ *@Exceptions: "N/A"
+ *@Version : "1.0"
+ *@See : "java.lang.Exception"
+ */
+public class RegistrationnotdoneException extends Exception {
+    public RegistrationnotdoneException(String message) {
+        super(message);
+    }
+}
